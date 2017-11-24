e9ee9d5d2f21578ffc3d07c429ebcd6df7432fa6
yunfeiroy666 <yunfeiroy@gmail.com>
2017-11-24 21:21:48 +0800

    modify gitignore

--- local-jira-auth-test.py
+++ local-jira-auth-test.py
@@ -22,16 +22,3 @@ print res_sessionid.status_code
 print res_sessionid.url
 print res_sessionid.text
 print res_sessionid.headers
-
-# url_use_sessionid = 'http://172.16.10.176:8080/rest/api/2/issue/TECH-6'
-# header1 = {
-#     "content-type": 'application/json',
-#     'cookie': 'JSESSIONID=6B43D4B4FCA9C257C1ED628B7F5895C2',
-#     "Accept": "application/json"
-# }
-# res_use_sessionid = requests.get(url=url_use_sessionid, headers=header1)
-#
-# print res_use_sessionid.status_code
-# print res_use_sessionid.url
-# print res_use_sessionid.text
-# print res_use_sessionid.headers
