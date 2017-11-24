cbd04370dc2d49171353d7f4bb3bfd27bfbb1f38
yunfeiroy666 <yunfeiroy@gmail.com>
2017-11-24 22:12:58 +0800

    modify jira-auth and other

--- jira-auth-test.py
+++ jira-auth-test.py
@@ -20,6 +20,10 @@ header = {'content-type': "application/json"}
 
 res_sessionid = requests.post(url=url_sessionid, data=json.dumps(param), headers=header)
 
+'''
+curl -D- -u jira:jira -X GET -H "Content-Type: application/json" http://127.0.0.1:8080/rest/auth/1/session
+'''
+
 print res_sessionid.status_code
 print res_sessionid.url
 print res_sessionid.text
--- jira-create-issue.py
+++ jira-create-issue.py
@@ -3,20 +3,22 @@ import requests
 import json
 from requests.auth import HTTPBasicAuth
 
-'''
-使用第一种认证方式
-'''
-
+# 最简单的用户名密码认证方式
 url1 = 'http://127.0.0.1.:8080/rest/auth/1/session'
 param1 = {'username': 'jira', 'password': 'github_jira'}
 
+# base64认证方式
 sub_url1 = 'http://127.0.0.1:8080/rest/api/2/issue/createmeta'
-header1 = {'content-type': 'application/json', 'Authorization': 'Basic amlyYTphaXNwZWVjaA=='}  # Authorization:base64编码
+header1 = {'content-type': 'application/json',
+           'Authorization': 'Basic amlyYTpnaXRodWJfamlyYQ=='}  # base64.b64encode(b'jira:github_jira')
 
-get_consum_fieldid_url = 'http://127.0.0.1:8080/rest/api/2/field'
+get_consum_fieldid_url = 'http://127.0.0.1:8080/rest/api/2/field'  # 获得自定义字段id，调用创建issue-api时需要用到
 
-url_create_issue = 'http://127.0.01:8080/rest/api/2/issue/'
+url_create_issue = 'http://127.0.01:8080/rest/api/2/issue/'  # 创建issue-api
 
+'''
+customfield_xxxxx,由Jira自动生成，对应某个创建的自定义字段，如客户联系方式等
+'''
 param2 = {
     "fields": {
         "project":
@@ -34,7 +36,7 @@ param2 = {
         'customfield_10001': "JIRA-GITHUB",
     }
 }
-header2 = {'content-type': 'application/json', 'Authorization': 'Basic YWRtaW46YWlzcGVlY2g=',  # admin:aispeech
+header2 = {'content-type': 'application/json', 'Authorization': 'Basic amlyYTpnaXRodWJfamlyYQ==',  # admin:aispeech
            'Accept': 'application/json'}
 
 header3 = {'content-type': 'application/json',
@@ -43,11 +45,12 @@ header3 = {'content-type': 'application/json',
 
 search_url = 'http://127.0.0.1:8080/rest/api/2/issue/TECH-6'
 
-# res = requests.session()
-# res = requests.post(url=url_create_issue, headers=header3, data=json.dumps(param2))
-
 res = requests.get(url=get_consum_fieldid_url, headers=header2)  # 获得该项目下所有字段等信息，json格式
 print res.status_code
 print res.url
 print res.text
 print res.headers
+
+# 创建issue，将获得的JSESSIONID放到请求头的cookie
+# res = requests.session()
+# res = requests.post(url=url_create_issue, headers=header3, data=json.dumps(param2))
